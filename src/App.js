3166657b099aa8e8a90d3127bd2dac50dfd9ea12
anthonylegrand <anthony-legrand@live.fr>
2022-11-23 15:08:32 +0100

    💄 Add Application_Mobile Section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './component/Navbar';
 import Home from './component/Home';
 import Service from './component/Service';
 import Menu from './component/Menu';
+import ApplicationMobile from './component/ApplicationMobile';
 
 function App() {
   const [currentPage, setPage] = useState('/')
@@ -27,6 +28,12 @@ function App() {
       <section className='flex' style={{ height: 'auto', margin: '5em 0' }}>
 
         <Menu />
+
+      </section>
+
+      <section className='flex jc-center ai-center colored'>
+
+        <ApplicationMobile />
 
       </section>
 
---
+++ src/component/ApplicationMobile.js
@@ -0,0 +1,29 @@
+import './../css/ApplicationMobile.css'
+
+import phone from './../img/stickers/phone.png'
+import google from './../img/stickers/google.png'
+import apple from './../img/stickers/apple.png'
+
+function ApplicationMobile() {
+
+  return (
+    <div id='Application_Mobile' className='flex f-row ai-center jc-space-between page-padding'>
+
+      <img src={phone} alt='Phone' />
+
+      <div>
+        <lore className='title'>Télécharger l’application</lore>
+        <h3>Télécharger l’application.<br/>Pour des commandes plus faciles</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        
+        <div className='flex f-row'>
+          <img src={google} alt='Google' />
+          <img src={apple} alt='Apple' />
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default ApplicationMobile;
