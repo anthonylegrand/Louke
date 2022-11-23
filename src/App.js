3c67adeb7e734c82411e8c727fc8d8cc8155e91e
anthonylegrand <anthony-legrand@live.fr>
2022-11-23 11:43:16 +0100

    💄 Add Home Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import './css/App.css';
 
 import Navbar from './component/Navbar';
+import Home from './component/Home';
 
 function App() {
   const [currentPage, setPage] = useState('/')
 
   return (
-    <div className="App">
-      <Navbar currentPage={currentPage} />
+    <div>
+
+      <section className='flex f-col' style={{ paddingTop: '2em' }}>
+        <Navbar currentPage={currentPage} />
+
+        <Home />
+      </section>
+
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,28 @@
+import './../css/Home.css'
+import Truck_svg from './../img/truck.svg'
+
+function Home() {
+
+  return (
+    <div id='Home' className='flex f-row jc-space-between ai-center'>
+        <div className='page-padding'>
+
+            <div className='tag'>
+                <label>The best eat ! 🔥</label>
+            </div>
+
+            <h2>Découvrire les meilleurs FoodTruck de ta ville. 100% <b>Pankackes</b></h2>
+            <h4 className='secondary-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h4>
+
+            <div className='flex f-row'>
+                <a className='button red' href='/#Menu'>Le Menu</a>
+                <a className='button' href='/#Service'>Nos Services</a>
+            </div>
+        </div>
+        
+        <img src={Truck_svg} alt='Looke logo' />
+    </div>
+  );
+}
+
+export default Home;
