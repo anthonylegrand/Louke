08aa8283a3557f107a1307de77f44817634619a6
NicolasHAMY <nhamypro@gmail.com>
2023-03-16 16:08:38 +0100

    Add recipes mocks & Adapt modal with recipes

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -16,7 +16,7 @@ const LIST = [
         icon: <FaFish/>
     },
     {
-        title: 'Pankakes',
+        title: 'Pancakes',
         icon: <FaHamburger/>
     },
     {
@@ -41,43 +41,195 @@ const pancakes = [
   {
     id: 1,
     image: "",
-    title: "Mon pancake 1",
-    weight: "110g",
-    price: "10 €",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    title: "Original Pancake Salé",
+    weight: "75g",
+    price: "7€",
+    recipe: [
+      "50g de farine",
+      "2 oeufs",
+      "60ml de lait",
+      "1 petite cuillère à soupe d'huile d'olive",
+      "1/2 sachet de levure chimique",
+      "1 pincée de sel",
+      "20g de fromage râpé",
+      "Morceaux de poivron rouge coupé en petits dés",
+      "Morceaux de lardons",
+      "30g de maïs doux ",
+    ],
+    filter: ["Pancake"]
   },
   {
     id: 2,
     image: "",
-    title: "Mon pancake 2",
-    weight: "110g",
-    price: "10 €",
-    description: "Voici la description de mon pancakes"
+    title: "Original Pancake Sucré",
+    weight: "60g",
+    price: "5€",
+    recipe: [
+      "2 œufs",
+      "1 banane",
+      "100g de flocon d’avoine",
+      "50ml d’arôme de vanille",
+      "Pincée de cannelle",
+      "20g de pépites de chocolat noir"
+
+    ],
+    filter: ["Dessert", "Accompagnement"]
   },
   {
     id: 3,
     image: "",
-    title: "Mon pancake 3",
-    weight: "110g",
-    price: "10 €",
-    description: "Voici la description de mon pancakes"
+    title: "Original Pancake Salé Veggie",
+    weight: "65g",
+    price: "6€50",
+    recipe: [
+      "50g de farine",
+      "1/2 cuillères à soupe de levure chimique",
+      "1/2 cuillère à café de sel",
+      "1 cuillères à soupe d'huile végétale",
+      "75ml de lait de soja",
+      "Morceaux de courgette râpée",
+      "1 petit oignon rouge râpé",
+      "1 petite carotte râpée",
+      "1/2 cuillère à café de cumin en poudre",
+    ],
+    filter: ["Pancake"]
   },
   {
     id: 4,
     image: "",
-    title: "Mon pancake 4",
-    weight: "110g",
-    price: "10 €",
-    description: "Voici la description de mon pancakes"
+    title: "Pancake aux 4 épices",
+    weight: "60g",
+    price: "4€",
+    recipe: [
+      "50g de farine",
+      "1/2 cuillères à soupe de levure chimique",
+      "1/4 cuillère à café de sel",
+      "1/2 cuillères à soupe de sucre de canne",
+      "75ml de lait d’amande",
+      "1 cuillère à café d’arôme de vanille",
+      "Pincée de cannelle",
+      "1/2 cuillère à café de muscade en poudre",
+      "1/2 cuillère à café de gingembre en poudre",
+      "1 pincée de clou de girofle en poudre",
+
+    ],
+    filter: ["Promotion"]
   },
   {
     id: 5,
     image: "",
-    title: "Mon pancake 5",
-    weight: "110g",
-    price: "10 €",
-    description: "Voici la description de mon pancakes"
-  }
+    title: "Pancake Duo Fromages",
+    weight: "65g",
+    price: "6€50",
+    recipe: [
+      "50g de farine",
+      "1/2 cuillères à soupe de levure chimique",
+      "1/2 cuillère à café de sel",
+      "75ml de lait",
+      "20g de bacon",
+      "2 oeuf",
+      "Morceaux de gruyère",
+      "1/2 tasse de ricotta",
+      "2 cuillères à soupe de ciboulette fraîche hachée",
+      "1 pincée de poivre noir"
+
+    ],
+    filter: ["Pancake", "Accompagnement"]
+  },
+  {
+    id: 6,
+    image: "",
+    title: "Pancake Chèvre Miel",
+    weight: "75g",
+    price: "5€50",
+    recipe: [
+      "50g de farine",
+      "1/2 cuillères à soupe de levure chimique",
+      "1/2 cuillère à café de sel",
+      "75ml de lait",
+      "1/2 cuillères à soupe d'huile végétale",
+      "2 oeuf",
+      "25g de fromage de chèvre frais",
+      "1 cuillère à soupe de miel liquide",
+      "1 pincée de poivre noir"
+    ],
+    filter: ["Promotion"]
+  },
+  {
+    id: 7,
+    image: "",
+    title: "Pancake Pistaches Framboises",
+    weight: "60g",
+    price: "5€",
+    recipe: [
+      "50g de farine",
+      "1/2 cuillères à soupe de levure chimique",
+      "1/2 cuillère à café de sel",
+      "75ml de lait d’amande",
+      "2 oeuf",
+      "1/4 de tasse de poudre de pistache",
+      "1/2 tasse de framboises fraîches ou surgelées",
+      "1 cuillère à soupe de sucre"
+    ],
+    filter: ["Dessert"]
+  },
+  {
+    id: 8,
+    image: "",
+    title: "Pancake Moment Café",
+    weight: "65g",
+    price: "4€",
+    recipe: [
+      "50g de farine",
+      "2 oeufs",
+      "60ml de lait",
+      "1 petite cuillère à soupe d'huile d'olive",
+      "1/2 sachet de levure chimique",
+      "1 pincée de sel",
+      "20g de fromage râpé",
+      "Morceaux de poivron rouge coupé en petits dés",
+      "Morceaux de lardons",
+      "30g de maïs doux ",
+    ],
+    filter: ["Dessert", "Accompagnement"]
+  },
+  // {
+  //   id: 9,
+  //   image: "",
+  //   title: "Pancake Pause Speculoos",
+  //   weight: "60g",
+  //   price: "4€50",
+  //   recipe: [
+  //     "25g de farine",
+  //     "1/8 de sachet de levure chimique",
+  //     "1 pincée de sel",
+  //     "1/2 cuillère à soupe de sucre",
+  //     "1/2 oeuf",
+  //     "40ml de lait d’avoine",
+  //     "1/2 cuillère à soupe d'huile végétale",
+  //     "1 cuillère à soupe de pâte de speculoos",
+  //   ],
+  //   filter: ["Promotion"]
+  // },
+  // {
+  //   id: 10,
+  //   image: "",
+  //   title: "Pancake Atlantique",
+  //   weight: "75g",
+  //   price: "7€",
+  //   recipe: [
+  //     "25g de farine",
+  //     "1/2 cuillère à café de levure chimique",
+  //     "1 pincée de sel",
+  //     "1/2 oeuf",
+  //     "40ml de lait",
+  //     "1/2 cuillère à soupe d'huile végétale",
+  //     "30g de saumon fumé",
+  //     "1 cuillère à soupe de ciboulette ciselée",
+  //     "Poivre noir"
+  //   ],
+  //   filter: ["Pancake"]
+  // },
 ]
 
 function Menu() {
--- src/component/MenuArticle.js
+++ src/component/MenuArticle.js
@@ -3,6 +3,18 @@ import Modal from 'react-modal';
 
 import pancakes from './../img/stickers/pancakes.svg'
 
+// Images pancakes
+import pancake1 from '../img/pancakes/1.png'
+import pancake2 from '../img/pancakes/2.png'
+import pancake3 from '../img/pancakes/3.png'
+import pancake4 from '../img/pancakes/4.png'
+import pancake5 from '../img/pancakes/5.png'
+import pancake6 from '../img/pancakes/6.png'
+import pancake7 from '../img/pancakes/7.png'
+import pancake8 from '../img/pancakes/8.png'
+import pancake9 from '../img/pancakes/9.png'
+import pancake10 from '../img/pancakes/10.png'
+
 import './../css/MenuArticle.css'
 
 const customStyles = {
@@ -42,43 +54,91 @@ function MenuArticle(props) {
     subtitle.style.color = '#f00';
   }
 
+  const getImg = (id) => {
+    let img = "";
+    switch (id) {
+      case 1:
+        img = pancake1;
+        break;
+
+      case 2:
+        img = pancake2;
+        break;
+      case 3:
+        img = pancake3;
+        break;
+
+      case 4:
+        img = pancake4;
+        break;
+
+      case 5:
+        img = pancake5;
+        break;
+
+      case 6:
+        img = pancake6;
+        break;
+
+      case 7:
+        img = pancake7;
+        break;
+
+      case 8:
+        img = pancake8;
+        break;
+
+      case 9:
+        img = pancake9;
+        break;
+
+      case 10:
+        img = pancake10;
+        break;
+
+      default:
+        break;
+    }
+
+    return img
+  }
 
 
   return (
     <article
-        className={'flex f-col jc-space-between ai-center'+(props.active_article === props.id ? ' acctive' : '')}
-        onMouseOver={()=>props.setActive(props.id)}>
-        {console.log(props.pancakes.title)}
-        {/* TO DO CHANGE IMG */}
-        <img src={pancakes} alt='pancakes icon' />
-        <h3>{props.pancakes.title}</h3>
-        <p>{props.pancakes.weight}</p>
+      className={'pancake-box flex f-col jc-space-between ai-center'+(props.active_article === props.id ? ' acctive' : '')}
+      onMouseOver={()=>props.setActive(props.id)}
+    >
+      <img src={getImg(props.pancakes.id)} alt='pancakes icon' />
+      <h3 className='pancake-title'>{props.pancakes.title}</h3>
+      <p>{props.pancakes.weight}</p>
+      <h3>{props.pancakes.price}</h3>
+      <button onClick={openModal}>Informations</button>
 
-        <h3>{props.pancakes.price}</h3>
-        <button onClick={openModal}>Information</button>
-
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          <div className='modal-content'>
-            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>{props.pancakes.title}</h2>
-            <img src={pancakes} alt='pancakes icon' />
-            <hr className='separator'/>
-            {/* <div className='hr-with-description'>
-              <hr className='separator'/>
-              <span>Description</span>
-            </div> */}
-            <div>
-              <h3 className='modal-description'>{props.pancakes.description}</h3>
-            </div>
-            <button className='modal-close-button' onClick={closeModal}>Retour</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className='modal-content'>
+          <h2 className='modal-title' ref={(_subtitle) => (subtitle = _subtitle)}>{props.pancakes.title}</h2>
+          <img src={getImg(props.pancakes.id)} alt='pancakes icon' />
+          <hr className='separator'/>
+          <div>
+            <ul className='list-ingredients'>
+              {props.pancakes.recipe.map((ingredient) => (
+                ingredient
+                  ? <li>{ingredient}</li>
+                  : ""
+              ))}
+            </ul>
           </div>
-        </Modal>
-    </article>
+          <button className='modal-close-button' onClick={closeModal}>Retour</button>
+        </div>
+      </Modal>
+  </article>
   );
 }
 
